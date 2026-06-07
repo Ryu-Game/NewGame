d7a99ee2cf0fe133d4b2370037119688ca98c6e0
ryuki-nyudo <83803844+ryuki-nyudo@users.noreply.github.com>
2026-06-07 10:05:42 +0900

    warning修正

--- BlockBreak/SRC/Menu.cpp
+++ BlockBreak/SRC/Menu.cpp
@@ -41,6 +41,6 @@ void Menu_Draw() {
 	SetFontSize(50);
 	DrawString((mgr.SCREEN_WIDTH / 2) - (50 * 3), mgr.SCREEN_HEIGHT / 2 - 150, "-BlockBreak-", 0xffffff);
 	DrawString((mgr.SCREEN_WIDTH / 2) - (50 * 4), mgr.SCREEN_HEIGHT / 2, "- PUSH [SPACE] -", 0xffffff);
-	DrawString((mgr.SCREEN_WIDTH / 2) - (50 * 4.5), mgr.SCREEN_HEIGHT / 2 + 150, "- PUSH [ESC] END -", 0xffffff);
+	DrawString((mgr.SCREEN_WIDTH / 2) - (int)(50 * 4.5), (mgr.SCREEN_HEIGHT / 2) + 150, "- PUSH [ESC] END -", 0xffffff);
 
 }
